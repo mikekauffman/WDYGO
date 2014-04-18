0971910fcbd94f0a6ab21024ed5912e72ee7855c
mikekauffman <mike.a.kauffman@gmail.com>
2014-04-18 16:39:16 -0600

    testing bug with https request using oauth

--- app.rb
+++ app.rb
@@ -1,28 +1,52 @@
 require 'sinatra/base'
+require 'oauth2'
 require 'json'
 require 'net/https'
 
 class Wdygo < Sinatra::Application
 
-  CHECKINS = []
+  TOKENS = []
   CLIENT_ID = 'PZZRWLEPHKV1CK2DAYZXZC1QJOHVTAJSGJPUBWYRCFPD0C5A'
   CLIENT_SECRET = 'HIB3WWLEBLE2VATGRMZUVOCZZ4NLWEMDCP4VTGDY55YSMDKK'
-  CALLBACK_PATH = 'http://wdygo.herokuapp.com/auth/foursquare/callback'
+  CALLBACK_PATH = '/auth/foursquare/callback'
+
+  def client
+    OAuth2::Client.new(CLIENT_ID, CLIENT_SECRET,
+                       {
+                         :site => 'http://foursquare.com/',
+                         :request_token_path => "/oauth2/request_token",
+                         :access_token_path => "/oauth2/access_token",
+                         :authorize_path => "/oauth2/authenticate?response_type=code",
+                         :parse_json => true,
+                         #:ssl => {:ca_path => '/etc/ssl/certs'}
+                       }
+    )
+  end
 
   get '/' do
-    CHECKINS << params[:access_token]
-    auth_link = "https://foursquare.com/oauth2/authenticate?client_id=#{CLIENT_ID}&response_type=token&redirect_uri=#{CALLBACK_PATH}"
-    erb :index, locals: {checkins: CHECKINS, authorize: auth_link}
+    auth_link = "https://foursquare.com/oauth2/authenticate?client_id=#{CLIENT_ID}&response_type=code&redirect_uri=#{redirect_uri}"
+    erb :index, locals: {tokens: TOKENS, authorize: auth_link}
   end
 
   get '/privacy' do
     "You're data is never sold"
   end
-  '/auth/foursquare/callback#access_token=AQ3DDXQTSF5L5Y0ZNQB03RC1L1DSHYLNI51YLO5E4Q3ISDEO'
 
-  get '/auth/foursquare/callback#access_token=:access_token' do
-    access_token = params[:access_token]
-    CHECKINS << access_token
+  get '/auth/foursquare/callback' do
+    p "BROKE BEFORE IF BLOCK"
+    if params[:code] != nil
+      p "GOT INSIDE IF BLOCK HERE: #{params[:code]}"
+      access_token = client.auth_code.get_token(params[:code], :redirect_uri => redirect_uri)
+    else
+      p "NO PARAMS CODE HERE:"
+      'Missing response from foursquare'
+    end
+      p "GOT THROUGH THE IF BLOCK HERE: #{params[:code]}"
+
+    #user = access_token.get('https://api.foursquare.com/v2/users/self')
+    #user.inspect
+
+    TOKENS << access_token
     redirect '/'
   end
 
@@ -30,26 +54,11 @@ class Wdygo < Sinatra::Application
     "Yep"
   end
 
-  #def redirect_uri
-  #  uri = URI.parse(request.url)
-  #  uri.path = CALLBACK_PATH
-  #  uri.query = nil
-  #  uri.to_s
-  #end
-  #'https://foursquare.com/oauth2/access_token
-  #?client_id=YOUR_CLIENT_ID
-  #&client_secret=YOUR_CLIENT_SECRET
-  #&grant_type=authorization_code
-  #&redirect_uri=YOUR_REGISTERED_REDIRECT_URI
-  #&code=CODE'
-  #def client
-  #  OAuth2::Client.new(CLIENT_ID, CLIENT_SECRET,
-  #                     :site => 'http://foursquare.com/v2/',
-  #                     :request_token_path => "/oauth2/request_token",
-  #                     :access_token_path  => "/oauth2/access_token",
-  #                     :authorize_path     => "/oauth2/authenticate?response_type=code",
-  #                     :parse_json => true
-  #  )
-  #end
+  def redirect_uri()
+    uri = URI.parse(request.url)
+    uri.path = CALLBACK_PATH
+    uri.query = nil
+    uri.to_s
+  end
 
 end
